f5cc3019a0c65f08c7c7873d57c03d986db5ee07
jordan_brewer <jordanbrewer1994@gmail.com>
2024-11-28 10:48:34 +0000

    retrieving categories from db

--- main.js
+++ main.js
@@ -5,6 +5,32 @@ const mysql = require("promise-mysql");
 const app = express();
 const port = 3001;
 
-app.get("/categories", category);
+const dbSettings = {
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "furniture_store",
+};
+app.get("/categories", async (req, res) => {
+  try {
+    const db = await mysql.createConnection(dbSettings);
+    const output = await db.query(
+        'SELECT `id`, `category` ' +
+        'FROM `categories`'
+    );
+    
+    const categories = output.map((row) => ({
+      id: row.id,
+      name: row.category,
+    }));
+    res.json({
+      message: "Successfully retrieved categories.",
+      data: categories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "An error occurred.", error: err.message });
+  }
+});
 
 app.listen(port);
