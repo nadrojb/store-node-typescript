81414a743d438f83b8e1d73421575832f08bc4ff
jordan_brewer <jordanbrewer1994@gmail.com>
2024-11-29 10:52:42 +0000

    now able to get single products in json

--- main.js
+++ main.js
@@ -12,4 +12,34 @@ app.get("/categories", categoriesController.categories);
 
 app.get("/products", allProductsController.allProducts);
 
+app.get("/product", async (req, res) => {
+  let productId = req.query.id;
+
+  try {
+    const db = await mysql.createConnection(dbSettings);
+    const productRow = await db.query(
+      `SELECT products.id, products.name, products.width, products.height, products.depth, products.price, products.stock, products.related, products.color, categories.id as categoryId FROM products LEFT JOIN categories ON products.name = categories.category  WHERE products.id = ?`,
+      [productId]
+    );
+
+    const product = productRow.map((productRow) => ({
+      categoryId: productRow.categoryId,
+      width: productRow.width,
+      height: productRow.height,
+      depth: productRow.depth,
+      price: productRow.price,
+      stock: productRow.stock,
+      related: productRow.related,
+      color: productRow.color,
+    }));
+    res.json({
+      message: "Successfully retrieved product.",
+      data: product,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Invalid product id", data: [] });
+  }
+});
+
 app.listen(port);
