b5ed3b68106a4017c3b1e9e0fed43467f27873f8
jordan_brewer <jordanbrewer1994@gmail.com>
2024-11-29 12:06:42 +0000

    now displaying data for one products

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mysql = require("promise-mysql");
 const categoriesController = require("./src/Controllers/categoriesController");
 const allProductsController = require("./src/Controllers/allProductsController");
+const singleProductController = require("./src/Controllers/singleProductController");
 const dbSettings = require("./environments");
 const app = express();
 const port = 3001;
@@ -12,34 +13,6 @@ app.get("/categories", categoriesController.categories);
 
 app.get("/products", allProductsController.allProducts);
 
-app.get("/product", async (req, res) => {
-  let productId = req.query.id;
-
-  try {
-    const db = await mysql.createConnection(dbSettings);
-    const productRow = await db.query(
-      `SELECT products.id, products.name, products.width, products.height, products.depth, products.price, products.stock, products.related, products.color, categories.id as categoryId FROM products LEFT JOIN categories ON products.name = categories.category  WHERE products.id = ?`,
-      [productId]
-    );
-
-    const product = productRow.map((productRow) => ({
-      categoryId: productRow.categoryId,
-      width: productRow.width,
-      height: productRow.height,
-      depth: productRow.depth,
-      price: productRow.price,
-      stock: productRow.stock,
-      related: productRow.related,
-      color: productRow.color,
-    }));
-    res.json({
-      message: "Successfully retrieved product.",
-      data: product,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Invalid product id", data: [] });
-  }
-});
+app.get("/product", singleProductController.singleProduct);
 
 app.listen(port);
---
+++ src/Controllers/singleProductController.js
@@ -0,0 +1,39 @@
+const mysql = require("promise-mysql");
+const dbSettings = require("../../environments");
+
+const singleProduct = async (req, res) => {
+  let productId = req.query.id;
+
+  try {
+    const db = await mysql.createConnection(dbSettings);
+    const productRow = await db.query(
+      `SELECT products.id, products.name, products.width, products.height, products.depth, products.price, products.stock, products.related, products.color, categories.id as categoryId FROM products LEFT JOIN categories ON products.name = categories.category  WHERE products.id = ?`,
+      [productId]
+    );
+
+    const productData = productRow[0];
+    const product = {
+      categoryId: productData.categoryId,
+      width: productData.width,
+      height: productData.height,
+      depth: productData.depth,
+      price: formatPrice(productData.price),
+      stock: productData.stock,
+      related: productData.related,
+      color: productData.color,
+    };
+    res.json({
+      message: "Successfully retrieved product.",
+      data: product,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Invalid product id", data: [] });
+  }
+};
+
+function formatPrice(input) {
+  return input.toFixed(2);
+}
+
+module.exports = { singleProduct };
