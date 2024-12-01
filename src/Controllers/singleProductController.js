af8c52029504941a44fbfe4d3372516438bb11e8
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-01 11:19:07 +0000

    Added conversion function for units of mesurement
    function is currently in single product controller

--- src/Controllers/singleProductController.js
+++ src/Controllers/singleProductController.js
@@ -3,6 +3,7 @@ const dbSettings = require("../../environments");
 
 const singleProduct = async (req, res) => {
   let productId = req.query.id;
+  let unit = req.query.unit;
 
   try {
     const db = await mysql.createConnection(dbSettings);
@@ -13,9 +14,9 @@ const singleProduct = async (req, res) => {
     const productData = productRow[0];
     const product = {
       categoryId: productData.categoryId,
-      width: productData.width,
-      height: productData.height,
-      depth: productData.depth,
+      width: convertUnit(unit, productData.width),
+      height: convertUnit(unit, productData.height),
+      depth: convertUnit(unit, productData.depth),
       price: formatPrice(productData.price),
       stock: productData.stock,
       related: productData.related,
@@ -33,6 +34,21 @@ const singleProduct = async (req, res) => {
 
 function formatPrice(input) {
   return input.toFixed(2);
+}
+
+function convertUnit(unit, input) {
+  if (unit == "mm") {
+    return input;
+  } else if (unit == "cm") {
+    let result = input * 0.1;
+    return result.toFixed(2);
+  } else if (unit == "in") {
+    let result = input * 0.0393701;
+    return result.toFixed(2);
+  } else if (unit == "ft") {
+    let result = input * 0.00328084;
+    return result.toFixed(2);
+  }
 }
 
 module.exports = { singleProduct };
